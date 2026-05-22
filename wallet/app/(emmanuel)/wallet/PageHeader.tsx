9d0e41819a49918065049d3596928ab2a8cf8815
Emmanuel Yakubu <emmanuelyakubu317@gmail.com>
2026-05-22 12:26:10 +0100

    feat: created PageHeader

---
+++ wallet/app/(emmanuel)/wallet/PageHeader.tsx
@@ -0,0 +1,11 @@
+
+export default function PageHeader() {
+  return (
+    <div className="flex items-center justify-between mb-8">
+      <p className="text-sm font-semibold text-gray-700">Welcome back!</p>
+      <h1 className="text-2xl font-bold tracking-tight">
+        <span className="text-green-700">V</span>ale
+      </h1>
+    </div>
+  )
+}
